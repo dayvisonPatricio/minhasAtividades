940a1c00c9edd7b8ffef85f7ba467dc95a4c297c
dayvisonPatricio <128934136+dayvisonPatricio@users.noreply.github.com>
2023-03-28 19:49:57 -0300

    Delete Q9 directory

--- Q9/script.js
+++
@@ -1,7 +0,0 @@
-const numerosImparesDiv = document.getElementById("numeros-impares");
-
-for (let i = 1; i <= 50; i++) {
-  if (i % 2 === 1) {
-    numerosImparesDiv.innerHTML += i + "<br>";
-  }
-}
